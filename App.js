bc1b1f4826c40608aabb7c0dc47a672f989fa6b6
ViniciusZancanari <viniciuszanca@hotmail.com>
2024-05-24 16:43:58 -0300

    add/Page - Agendamento de Aula 5

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import VerificarEmail from './src/page/VerificarEmail';
+import AgendamentoAula10 from './src/page/AgendamentoAula10';
+import AgendamentoAula8 from './src/page/AgendamentoAula8';
+import AgendamentoAula7 from './src/page/AgendamentoAula7';
+import AgendamentoAula6 from './src/page/AgendamentoAula6';
+import AgendamentoAula9 from './src/page/AgendamentoAula9';
+import AgendamentoAula1 from './src/page/AgendamentoAula1';
+import AgendamentoAula4 from './src/page/AgendamentoAula4';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AgendamentoAula4/>
   );
 }
 
---
+++ src/page/AgendamentoAula5.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+const AgendamentoAula5 = () => {
+  return (
+    <LinearGradient colors={['#E83378', '#F47920']} style={styles.container}>
+      <Text style={styles.subtitle}>Você selecionnou o dia<Text style={styles.subtitle2}>17/08/23 às 15h</Text></Text>
+      <View style={styles.image}>
+        <Image
+          source={require('../../assets/perfil.png')}
+        />
+        <Image
+          source={require('../../assets/grafismo.png')}
+        />
+      </View>
+
+      <Text style={styles.title}>Thiago Oliveira Freitas</Text>
+      <Text style={styles.subtitle}>Local de encontro:</Text>
+      
+      <View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Utilizar o endereço cadastrado</Text>
+        </TouchableOpacity>
+        <Text style={styles.title}>ou</Text>
+        <TouchableOpacity >
+          <Text style={styles.marcarButton}>Marcar local de encontro</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TextInput
+      style={styles.input}
+      placeholder="Digite um endereço"
+      placeholderTextColor="white"
+      selectionColor="white"
+    />
+
+      <View style={styles.rowButton}>
+      <TouchableOpacity style={styles.voltarButton}>
+          <Text style={styles.buttonText}>Voltar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.avancarButton}>
+          <Text style={styles.buttonText}>Avançar</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    width: '100%',
+  },
+
+  title: {
+    fontSize: 24,
+    color: '#FFF',
+    textAlign: 'center',
+    marginBottom:20
+    
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#FFF',
+    textAlign: 'center',
+    marginBottom:50
+  },
+  subtitle2: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'yellow',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+
+  button: {
+    backgroundColor: '#191970',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+  },
+  marcarButton:{
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+    color: '#000',
+    marginBottom:50
+  },
+  
+  rowButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+
+  voltarButton:{
+    marginRight: 20,
+    paddingVertical: 8,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#fff',
+    marginBottom: 50,
+    color: '#FFF'
+  },
+
+  avancarButton:{
+    backgroundColor: '#191970',
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    borderRadius: 30,
+    marginBottom: 50,
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderColor: '#faac0f'
+
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+
+  },
+  image: {
+    width: 100, // Adjust the width as needed
+    height: 100, // Adjust the height as needed
+    flex: 1,
+    flexDirection: 'row',
+    top: 40,
+    left: 0
+  }
+});
+
+export default AgendamentoAula5;

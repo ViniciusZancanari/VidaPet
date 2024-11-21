61ff2c58f166332ca3dc6693e0f00625250675be
ViniciusZancanari <viniciuszanca@hotmail.com>
2024-11-21 19:21:52 -0300

    add/Page - Dados do Usuário - Back-end

--- front-end/src/app/page/Cadastro.js
+++ front-end/src/app/page/Cadastro.js
@@ -2,67 +2,132 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Checkbox from 'expo-checkbox';
+import axios from 'axios'
 import { Link } from 'expo-router';
+;
 
 const Cadastro = () => {
   const [isSelected, setSelection] = useState(false);
   const [isSMSSelected, setSMSSelection] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [email, setEmail] = useState('');
+  const [CPF, setCPF] = useState('');
+  const [address, setAddress] = useState('');
+  const [cellphone, setCellphone] = useState('');
+
+  const handleRegister = () => {
+    if (password !== confirmarSenha) {
+      alert('As senhas não coincidem');
+      return;
+    }
+
+    axios.post('http://192.168.0.2:3000/client/', {
+      username,
+      password,
+      email,
+      CPF,
+      address,
+      cellphone,
+      privacy_policy: isSelected,
+      sms_whatsapp_messages: isSMSSelected,
+    })
+      .then(response => {
+        console.log(response.data);
+        alert('Cadastro realizado com sucesso!');
+      })
+      .catch(error => {
+        console.error('Erro na requisição:', error.response ? error.response.data : error.message);
+        alert('Erro ao realizar o cadastro: ' + (error.response ? error.response.data : error.message));
+      });
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
       <Image style={styles.logo} source={require('../../../assets/logo1.png')} />
-      <View style={styles.radioContainer}>
-        <TouchableOpacity style={styles.radioItem}>
-          <View style={styles.radioButtonSelected} />
-          <Text style={styles.radioText}>Usuário</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.radioItem}>
-          <View style={styles.radioButton} />
-          <Text style={styles.radioText}>Adestrador</Text>
-        </TouchableOpacity>
-      </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>E-mail:</Text>
-        <TextInput style={styles.input} placeholder="E-mail" placeholderTextColor="#FFF" />
+        <Text style={styles.inputLabel}>Nome Completo:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome de usuário"
+          placeholderTextColor="#FFF"
+          value={username}
+          onChangeText={setUsername}
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Nome completo:</Text>
-        <TextInput style={styles.input} placeholder="Nome completo" placeholderTextColor="#FFF" />
+        <Text style={styles.inputLabel}>E-mail:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#FFF"
+          value={email}
+          onChangeText={setEmail}
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.inputContainer}>
         <Text style={styles.inputLabel}>Celular:</Text>
-        <TextInput style={styles.input} placeholder="Celular" placeholderTextColor="#FFF" />
+        <TextInput
+          style={styles.input}
+          placeholder="Celular"
+          placeholderTextColor="#FFF"
+          value={cellphone}
+          onChangeText={setCellphone}
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Digite uma senha:</Text>
-        <TextInput style={styles.input} placeholder="Digite uma senha" placeholderTextColor="#FFF" secureTextEntry={true} />
+        <Text style={styles.inputLabel}>Senha:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha:"
+          placeholderTextColor="#FFF"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.inputContainer}>
         <Text style={styles.inputLabel}>Confirmar senha:</Text>
-        <TextInput style={styles.input} placeholder="Confirmar senha" placeholderTextColor="#FFF" secureTextEntry={true} />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar senha"
+          placeholderTextColor="#FFF"
+          secureTextEntry={true}
+          value={confirmarSenha}
+          onChangeText={setConfirmarSenha}
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.checkboxContainer}>
-        <Checkbox value={isSelected} onValueChange={setSelection} color={isSelected ? '#4630EB' : undefined} />
+        <Checkbox
+          value={isSelected}
+          onValueChange={() => setSelection(!isSelected)}
+          color={isSelected ? '#4630EB' : undefined}
+        />
         <Text style={styles.label}>Li e estou de acordo com o Termo de Uso e Política de Privacidade</Text>
       </View>
 
       <View style={styles.checkboxContainer}>
-        <Checkbox value={isSMSSelected} onValueChange={setSMSSelection} color={isSMSSelected ? '#4630EB' : undefined} />
+        <Checkbox
+          value={isSMSSelected}
+          onValueChange={setSMSSelection}
+          color={isSMSSelected ? '#4630EB' : undefined}
+        />
         <Text style={styles.label}>Aceito receber mensagens via SMS e WhatsApp</Text>
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
         <Link style={styles.buttonText} href="/page/VerificarEmail">Confirmar</Link>
       </TouchableOpacity>
     </ScrollView>
@@ -81,36 +146,6 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     borderRadius: 0,
     marginBottom: 20,
-  },
-  radioContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-    marginBottom: 20,
-  },
-  radioItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  radioButton: {
-    height: 20,
-    width: 20,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 5,
-  },
-  radioButtonSelected: {
-    height: 10,
-    width: 10,
-    borderRadius: 5,
-    backgroundColor: '#FFF',
-  },
-  radioText: {
-    color: 'white',
-    marginLeft: 5,
   },
   inputContainer: {
     width: '90%',

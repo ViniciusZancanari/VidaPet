f28f28cc5fb80ef189618a14f554f40bd5ea1309
ViniciusZancanari <viniciuszanca@hotmail.com>
2025-01-12 18:44:47 -0300

    add/agendamento 7 - back-end

--- front-end/src/app/page/AgendamentoAula7.js
+++ front-end/src/app/page/AgendamentoAula7.js
@@ -1,66 +1,103 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Alert, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router'; 
+import axios from 'axios';
+import Constants from 'expo-constants';
+import { useAgendamento } from '../context/AgendamentoContext'; // Importando o contexto de agendamento
 
 const AgendamentoAula7 = () => {
-  return (
-    <LinearGradient colors={['#E83378', '#F47920']} style={styles.container}>
-      <View style={styles.grafismo}>
-        <Image
-          source={require('../../../assets/grafismo.png')}
-        />
-      </View>
-      <View style={styles.pixIconContainer}>
-        <Text style={styles.title}>Confirme o Pedido:</Text>
-        <Image
-          source={require('../../../assets/profissional.png')}
-        />
-      </View>
-      <View style={styles.line}>
-        <Text style={styles.subtitle}>Profissional:</Text>
-        <Text style={styles.instructions}>
-          Thiago Oliveira Freitas
-        </Text>
-      </View>
-      <Image
-        source={require('../../../assets/data.png')}
-      />
-      <View style={styles.line}>
-        <Text style={styles.subtitle}>Data/Horário:</Text>
-        <Text style={styles.instructions}>
-          17/08/23 às 15h
-        </Text>
-      </View>
-      <Image
-        source={require('../../../assets/local.png')}
-      />
-      <View style={styles.line}>
-        <Text style={styles.subtitle}>Local do Encontro:</Text>
-        <Text style={styles.instructions}>
-          Av. Lorem Ipsum, 135{'\n'} Aclimação - São Paulo/SP
-        </Text>
-      </View>
-      <View style={styles.buttons}>
-        <TouchableOpacity style={styles.dadosButton}>
-          <Link
-            style={styles.buttonText}
-            href="/page/AgendamentoAula1">Alterar Dados
-          </Link>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.confirmarButton}>
-          <Link
-            style={styles.buttonText}
-            href="/page/AgendamentoAula8">Confirmar
-          </Link>
-        </TouchableOpacity>
+  const router = useRouter();
+  //const [ip, setIp] = useState(Constants.manifest2?.extra?.localhost || '192.168.0.6');
+/*
+  useEffect(() => {
+   
 
-      </View>
-    </LinearGradient>
+  const handleConfirmar = async () => {
+    try {
+      if (!selectedDate || !selectedTime || selectedDate === '' || selectedTime === '') {
+        Alert.alert('Erro', 'Data ou horário não selecionados');
+        return;
+      }
+
+      const isoDateTime = convertDateTimeToISO(selectedDate, selectedTime);
+      console.log('ISO DateTime gerado:', isoDateTime); // Log da data ISO gerada
+
+      const trainingServiceData = {
+        client_id: "7a762d38-07e1-4ac8-b717-16a41cd523ec",
+        trainer_id: "6194a177-923d-4c03-8504-2ef51df5992e",
+        type_payment: "CARD",
+        address: "Av Jose Cunha , 382",
+        hourClass: selectedTime,
+        availableDate: isoDateTime,
+      };
+
+      const response = await axios.post(`http://${ip}:3000/trainingService`, trainingServiceData);
+      if (response.status === 200) {
+        Alert.alert('Sucesso', 'Agendamento confirmado!');
+        router.push('/page/AgendamentoAula8');
+      } else {
+        throw new Error('Erro ao confirmar o agendamento');
+      }
+    } catch (error) {
+      console.error('Erro ao confirmar agendamento:', error.message);
+      Alert.alert('Erro', error.message || 'Não foi possível confirmar o agendamento.');
+    }
+  };
+
+  */
+
+  return (
+    <ScrollView>
+      <LinearGradient colors={['#E83378', '#F47920']} style={styles.container}>
+        <View style={styles.header}>
+          <Link href="/page/PerfilAdestrador">
+            <Text style={styles.closeButtonText}>X</Text>
+          </Link>
+        </View>
+
+        <View style={styles.grafismo}>
+          <Image source={require('../../../assets/grafismo.png')} />
+        </View>
+        <View style={styles.pixIconContainer}>
+          <Text style={styles.title}>Confirme o Pedido:</Text>
+          <Image source={require('../../../assets/profissional.png')} />
+        </View>
+        <View style={styles.line}>
+          <Text style={styles.subtitle}>Profissional:</Text>
+          <Text style={styles.instructions}>Thiago Oliveira Freitas</Text>
+        </View>
+        <Image source={require('../../../assets/data.png')} />
+        <View style={styles.line}>
+          <Text style={styles.subtitle}>Data/Horário:</Text>
+          <Text style={styles.instructions}>01/11/2024 às 10 horas</Text>
+        </View>
+        <Image source={require('../../../assets/local.png')} />
+        <View style={styles.line}>
+          <Text style={styles.subtitle}>Local do Encontro:</Text>
+          <Text style={styles.instructions}>
+            Av. Lorem Ipsum, 135{'\n'} Aclimação - São Paulo/SP
+          </Text>
+          <View style={styles.line}>
+          <Image source={require('../../../assets/valorTrainner.png')} />
+          <Text style={styles.subtitle}>Valor do serviço:</Text>
+          <Text style={styles.instructions}>
+            R$ 50,00
+          </Text>
+        </View>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.dadosButton}>
+            <Text style={styles.buttonText}>Alterar Dados</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.confirmarButton}>
+            <Link style={styles.buttonText} href={'/page/AgendamentoAula8'}>Confirmar</Link>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
+    </ScrollView>
   );
 };
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -70,20 +107,28 @@ const styles = StyleSheet.create({
     padding: 20,
     width: '100%',
   },
-
+  header: {
+    position: 'absolute',
+    top: 40,
+    right: 20,
+  },
+  closeButtonText: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
   pixIconContainer: {
     marginTop: 150,
     marginBottom: 20,
     alignItems: 'center',
     justifyContent: 'center',
-
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
     color: '#FFF',
     textAlign: 'center',
-    marginBottom: 50
+    marginBottom: 50,
   },
   subtitle: {
     fontSize: 15,
@@ -91,9 +136,8 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: '#FFF',
     textAlign: 'center',
-    marginBottom: 20
+    marginBottom: 20,
   },
-
   instructions: {
     fontSize: 16,
     textAlign: 'center',
@@ -111,12 +155,11 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     marginBottom: 50,
     color: '#FFF',
-
   },
   line: {
     borderBottomWidth: 2,
     borderBottomColor: '#F27B61',
-    marginBottom: 20
+    marginBottom: 20,
   },
   buttons: {
     flexDirection: 'row',
@@ -132,22 +175,19 @@ const styles = StyleSheet.create({
     marginBottom: 50,
     borderWidth: 3,
     borderStyle: 'solid',
-    borderColor: '#faac0f'
-
+    borderColor: '#faac0f',
   },
-
   buttonText: {
     color: '#fff',
     fontSize: 16,
-
   },
   grafismo: {
-    width: 100, // Adjust the width as needed
-    height: 100, // Adjust the height as needed
+    width: 100,
+    height: 100,
     position: 'absolute',
     top: 70,
-    left: 0
-  }
+    left: 0,
+  },
 });
 
 export default AgendamentoAula7;

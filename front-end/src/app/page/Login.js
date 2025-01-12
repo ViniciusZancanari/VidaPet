e944d41d1577ec447c869545fc4ec6bbc38da0fe
ViniciusZancanari <viniciuszanca@hotmail.com>
2025-01-12 18:57:13 -0300

    add/login - back-end

--- front-end/src/app/page/Login.js
+++ front-end/src/app/page/Login.js
@@ -1,29 +1,65 @@
 import React, { useState } from 'react';
-import { Link } from 'expo-router';
-import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, StyleSheet, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import { Link } from 'expo-router'; // Pode ser usado para outras navegações
 
 export const Login = () => {
-  const [isSelected, setSelection] = useState(false);
-  const [isSMSSelected, setSMSSelection] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://192.168.0.6:3000/client/authenticate', {
+        email,
+        password,
+      });
+
+      // Verifique se o login foi bem-sucedido
+      if (response.status === 200) {
+        // Navegar para a página home após o login bem-sucedido
+        console.log("DEU BOM")
+      } else {
+        throw new Error('Falha no login');
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error.response ? error.response.data : error.message);
+      Alert.alert('Erro ao realizar o login: ' + (error.response ? error.response.data.message : error.message));
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Image style={styles.logo} source={require('../../../assets/logo1.png')} />
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Login:</Text>
-        <TextInput style={styles.input} placeholder="Login:" placeholderTextColor="#FFF" />
+        <Text style={styles.inputLabel}>E-mail:</Text>
+        <TextInput 
+          style={styles.input} 
+          placeholder="E-mail:" 
+          placeholderTextColor="#FFF" 
+          value={email} 
+          onChangeText={setEmail} 
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
       <View style={styles.inputContainer}>
         <Text style={styles.inputLabel}>Senha:</Text>
-        <TextInput style={styles.input} placeholder="Senha:" placeholderTextColor="#FFF" secureTextEntry />
+        <TextInput 
+          style={styles.input} 
+          placeholder="Senha:" 
+          placeholderTextColor="#FFF" 
+          secureTextEntry 
+          value={password} 
+          onChangeText={setPassword} 
+        />
         <LinearGradient colors={['#E83378', '#F47920']} style={styles.gradientLine} />
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Link style={styles.buttonText} href="/page/Home">Entrar</Link>
       </TouchableOpacity>
 
